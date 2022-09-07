72b55621ec07ba661c684a5d488dd7c8f0da10cf
francisyzy <francisyzy@u.nus.edu>
2022-09-08 03:36:19 +0800

    Add Psuedo Code

--- Simple Java Practice Questions/sortofsorting/sortofsorting.java
+++ Simple Java Practice Questions/sortofsorting/sortofsorting.java
@@ -94,3 +94,43 @@ class FastIO extends PrintWriter {
         return str;
     }
 }
+
+// FastIO fio = FastIO 
+// int students = fio.nextInt
+// while (students != 0) 
+//     ArrayList<student> studentList = new ArrayList<student>
+//     // Get all student names for that class
+//     for (int i = 0, i < students, i++) {
+//         studentList.add(new student(fio.next))
+//     }
+//     // Java sort is stable
+//     studentList.sort((s1, s2) -> s1.compare(s1, s2))
+//     for (student : (studentList)) {
+//         fio.println(student)
+//     }
+
+//     fio.println("")
+//     // Get next batch of student
+//     students = fio.nextInt
+        
+
+
+// class student: Comparator {
+//     String name
+
+//     student(name) {
+//         this.name:name
+//     }
+
+//     // the overridden compare method to sort the first two characters only
+//     @Override
+//     public int compare(s1, s2) {
+//         return s1.name.substring(0, 2).compareTo(s2.name.substring(0, 2))
+//     }
+
+//     @Override
+//     public String toString {
+//         return this.name
+//     }
+// }
+
