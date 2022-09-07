86431cceed477cc0d0cb9c41e38f47de4859d305
Francis Yeo <github@francisyzy.com>
2022-09-07 12:03:29 +0800

    Add comments and clean up unused code

--- Assignment/Assignment 1/cardtrading/cardtrading.java
+++ Assignment/Assignment 1/cardtrading/cardtrading.java
@@ -1,4 +1,3 @@
-
 ***REMOVED***
 import java.util.*;
 
@@ -9,39 +8,30 @@ public class cardtrading {
         int n = fio.nextInt();
         int t = fio.nextInt();
         int k = fio.nextInt();
+
+        //Create Deck ArrayList
         ArrayList<Integer> deck = new ArrayList<Integer>();
-        // Get distribution of deck
-        int[] cardFrequency = new int[t];
         deck.ensureCapacity(n);
+
+        // Get distribution of deck (frequency of card showing up in the deck)
+        int[] cardFrequency = new int[t];
+
         for (int i = 0; i < n; i++) {
             int cardId = fio.nextInt();
             deck.add(cardId);
             cardFrequency[cardId-1] += 1;
         }
-        
+
+        //Create Shop ArrayList        
         ArrayList<ShopItem> items = new ArrayList<ShopItem>();
         items.ensureCapacity(t);
         for (int i = 0; i < t; i++) {
             items.add(new ShopItem(i + 1, fio.nextInt(), fio.nextInt()));
         }
 
-        // deck.sort((c1, c2) -> c1.compareTo(c2));
-
-        // Get value of deck
-        // long valueOfDeck = 0;
-        // for (Integer deckCard : deck) {
-        // valueOfDeck += items.get(deckCard).sellPrice;
-        // }
-
-        // // Get distribution of deck
-        // int[] cardFrequency = new int[t];
-        // Set<Integer> distinct = new HashSet<Integer>(deck);
-        // for (Integer card : distinct) {
-        //     cardFrequency[card - 1] = Collections.frequency(deck, card);
-        // }
-
         // Get and set the opportunity cost of cards & set the frequency of cards
         for (int i = 0; i < cardFrequency.length; i++) {
+            //Set the count of deck into the shop card data
             items.get(i).setFrequency(cardFrequency[i]);
             if (cardFrequency[i] == 2) {
                 items.get(i).setOpportunityCost(items.get(i).sellPrice * 2);
@@ -53,30 +43,8 @@ public class cardtrading {
         }
 
         long cost = 0;
-        // items.sort((card1, card2) -> card1.compare(card1, card2));
+        //Sort the shop items by their opportunity cost
         Collections.sort(items);
-        // for (int i = 1; i < items.size(); i++) {
-        // if (i < k+1) {
-        // cost -= items.get(i).buyPrice;
-        // } else {
-        // cost += items.get(i).sellPrice;
-        // }
-        // }
-        // if (n < 1) {
-        // for (int i = 0; i < k; i++) {
-        // cost -= items.get(i).getBuyPrice();
-        // }
-        // } else {
-        // for (int i = 0; i < k; i++) {
-        // cost -= items.get(i).getBuyPrice();
-        // // System.out.println(items.get(i).getBuyPrice());
-        // }
-        // // System.out.println(cost);
-        // for (int i = k; i < items.size(); i++) {
-        // cost += items.get(i).getSellPrice();
-        // // System.out.println(items.get(i).getSellPrice());
-        // }
-        // }
         for (int i = 0; i < items.size(); i++) {
             if (i < k) {
                 cost -= items.get(i).getBuyPrice();
@@ -86,20 +54,6 @@ public class cardtrading {
         }
         fio.println(cost);
         fio.close();
-        // System.out.println(items.toString());
-
-        // Check if inputs are correct
-        // System.out.println(n);
-        // System.out.println(t);
-        // System.out.println(k);
-        // for (Integer deckItem : deck) {
-        // System.out.print(deckItem);
-        // }
-        // System.out.println("");
-        // for (ShopItem shopItem : items) {
-        // System.out.println(shopItem);
-        // }
-
     }
 }
 
@@ -116,6 +70,10 @@ class ShopItem implements Comparable<ShopItem> {
         this.sellPrice = sellPrice;
     }
 
+    /**
+    Get the buy price of the card. If the user already has 2 card, 2-0 * buy = 0 
+    as the user does not need to buy any cards.
+    */
     public long getBuyPrice() {
         return (2 - frequency) * this.buyPrice;
     }
@@ -132,6 +90,9 @@ class ShopItem implements Comparable<ShopItem> {
         this.frequency = frequency;
     }
 
+    /**
+    Comparing current shop item's opportunity cost and the incoming opportunity cost
+    */
     @Override
     public int compareTo(ShopItem card2) {
         if (this.opportunityCost > card2.opportunityCost) {
@@ -142,7 +103,9 @@ class ShopItem implements Comparable<ShopItem> {
             return 0;
         }
     }
-
+    /**
+    For debug when printing out the shop items
+    */
     @Override
     public String toString() {
         return cardId + " " + buyPrice + " " + sellPrice + " " + opportunityCost;
