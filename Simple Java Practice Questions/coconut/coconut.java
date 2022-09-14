1226330af143bfb852bb1a72ef16844959142024
francisyzy <francisyzy@u.nus.edu>
2022-09-15 02:11:41 +0800

    Add comments

--- Simple Java Practice Questions/coconut/coconut.java
+++ Simple Java Practice Questions/coconut/coconut.java
@@ -8,34 +8,33 @@ public class coconut {
         int syllables = fio.nextInt(); // read int
         int players = fio.nextInt(); // read int
 
+        // LL of hands that are in the game
         LinkedList<hand> ll = new LinkedList<hand>();
+        // Creates the players and adds them in order into the ll
         for (int i = 0; i < players; i++) {
-            // fio.println(ll.toString());
             ll.addLast(new hand(i));
         }
-        // fio.println(ll.toString());
-        // System.out.println(syllables);
-        // System.out.println(players);
         do {
-            // fio.println(ll.toString());
             for (int i = 0; i < syllables - 1; i++) {
                 hand move = ll.removeFirst();
                 ll.addLast(move);
             }
-            hand curr_hand = ll.removeFirst();
-            // fio.println(ll.toString());
-            
-            if (curr_hand.handType == "folded") {
-                // fio.println("ASD");
-                ll.addFirst(new hand(curr_hand.playerId));
-                ll.addFirst(new hand(curr_hand.playerId));
-            } else if (curr_hand.handType == "fist") {
-                ll.addLast(new hand(curr_hand.playerId, "palm"));
-            }
+            hand current_hand = ll.removeFirst();
+
+            // If the hand is folded, break the hand into two. Typings are auto inside the
+            // hand class
+            if (current_hand.handType == "folded") {
+                ll.addFirst(new hand(current_hand.playerId));
+                ll.addFirst(new hand(current_hand.playerId));
+            } else if (current_hand.handType == "fist") { // If its a fist, change it to a palm and the next hand starts
+                                                          // Thus the current hand will be added into the back
+                ll.addLast(new hand(current_hand.playerId, "palm"));
+            } // If its a palm, do nothing because it has already been removed from the ll
+
         } while (ll.size() > 1);
+        // Do while loop because the game needs to be played regardless
 
-        fio.println(ll.getFirst().playerId); // print the "..." contents with newline at the end
-
+        fio.println(ll.getFirst().playerId); // gets the first player and prints it
         fio.close(); // important; always close at the end of the code
     }
 }
@@ -44,25 +43,50 @@ class hand {
     public String playerId;
     public String handType;
 
+    /**
+     * Create hand with id + 1. Code i = 0 but id starts @ 1
+     * Hand of original state
+     * 
+     * @param id
+     */
     public hand(int id) {
         this.playerId = Integer.toString(id + 1);
         this.handType = "folded";
     }
 
+    /**
+     * Create hand using ID from string, means copying from a previous hand. If
+     * copying means the hand change to next state
+     * 
+     * @param id
+     */
     public hand(String id) {
         this.playerId = id;
         this.handType = "fist";
     }
 
+    /**
+     * Creates hand with given id and state of the hand
+     * 
+     * @param id
+     * @param type
+     */
     public hand(String id, String type) {
         this.playerId = id;
         this.handType = type;
     }
 
+    /**
+     * Was used to set the type of the hand so that new hands do not need to be
+     * created
+     * 
+     * @param type (current hand type)
+     */
     public void setHandType(String type) {
         this.handType = type;
     }
 
+    /** Used for debugging to print the playerId from the ll */
     @Override
     public String toString() {
         return playerId;
