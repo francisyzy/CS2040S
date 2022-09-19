45a6debfd2d8a91ef5e032a207a614a2882d9dfc
francisyzy <francisyzy@u.nus.edu>
2022-09-19 15:23:24 +0800

    First attempt
    TLE
    
    too slow

--- Assignment/Assignment 2/joinstrings/joinstrings.java
+++ Assignment/Assignment 2/joinstrings/joinstrings.java
@@ -5,12 +5,36 @@ public class joinstrings {
     public static void main(String[] args) {
         FastIO fio = new FastIO(); // create new instance
 
-        int num = fio.nextInt(); // read int
-        double real = fio.nextDouble(); // read double
-        String token = fio.next(); // read a single token
-        String line = fio.nextLine(); // read an entire line
-        fio.print("..."); // print the "..." contents
-        fio.println("..."); // print the "..." contents with newline at the end
+        int numOfStrings = fio.nextInt(); // read int
+
+        ArrayList<StringBuilder> strings = new ArrayList<StringBuilder>();
+        strings.ensureCapacity(numOfStrings);
+        for (int i = 0; i < numOfStrings; i++) {
+            strings.add(new StringBuilder(fio.nextLine()));
+        }
+
+        // String operations
+        for (int i = 0; i < numOfStrings - 1; i++) {
+            int first = fio.nextInt() - 1; // read int
+            int second = fio.nextInt() - 1; // read int
+            strings.set(first, strings.get(first).append(strings.get(second)));
+            strings.set(second, null);
+
+        }
+        // for (int i = 0; i < numOfStrings; i++) {
+        // if (strings.get(i) != null) {
+        // fio.println(strings.get(i));
+        // }
+        // }
+        // strings.removeAll(Collections.singleton(null));
+        strings.removeIf(Objects::isNull);
+        fio.println(strings.get(0));
+
+        // double real = fio.nextDouble(); // read double
+        // String token = fio.next(); // read a single token
+        // String line = fio.nextLine(); // read an entire line
+        // fio.print("..."); // print the "..." contents
+        // fio.println("..."); // print the "..." contents with newline at the end
 
         fio.close(); // important; always close at the end of the code
     }
