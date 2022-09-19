4f9ba344e7d5047241f04186ccf8e8fc8222c4d6
francisyzy <francisyzy@u.nus.edu>
2022-09-20 01:40:14 +0800

    Finish Teque

--- Assignment/Assignment 2/teque/teque.java
+++ Assignment/Assignment 2/teque/teque.java
@@ -1,3 +1,4 @@
+***REMOVED***
 import java.io.*;
 import java.util.*;
 
@@ -5,16 +6,118 @@ public class teque {
     public static void main(String[] args) {
         FastIO fio = new FastIO(); // create new instance
 
-        int num = fio.nextInt(); // read int
-        double real = fio.nextDouble(); // read double
-        String token = fio.next(); // read a single token
-        String line = fio.nextLine(); // read an entire line
-        fio.print("..."); // print the "..." contents
-        fio.println("..."); // print the "..." contents with newline at the end
+        int noOfOperations = fio.nextInt(); // read int
+
+        FakeCircularArrayList front = new FakeCircularArrayList(noOfOperations * 2);
+        FakeCircularArrayList back = new FakeCircularArrayList(noOfOperations * 2);
+        for (int i = 0; i < noOfOperations; i++) {
+            String operation = fio.next(); // read till next token of space bar
+            int number = fio.nextInt(); // Get the number to push into or get
+            // Fancy if else statement
+            switch (operation) {
+                case "push_front":
+                    front.addFront(number);
+                    // Check if both "ArrayList" are balanced
+                    if (front.size() > back.size() + 1) {
+                        // Rebalance it if its not balanced
+                        back.addFront(front.removeBack());
+                    }
+                    break;
+                case "push_middle":
+                    // Check which array to push middle values into
+                    if (front.size() < back.size() + 1) {
+                        front.addBack(number);
+                    } else {
+                        back.addFront(number);
+                    }
+                    break;
+                case "push_back":
+                    back.addBack(number);
+                    // Check if both "ArrayList" are balanced
+                    if (back.size() > front.size()) {
+                        // Rebalance it if its not balanced
+                        front.addBack(back.removeFront());
+                    }
+                    break;
+                default: // get operation
+                    // Check if the front is bigger than the queried number
+                    // If so, means number belong in front arr
+                    if (front.size() > number) {
+                        fio.println(front.get(number));
+                    } else {
+                        // Remove the len of front arr to get the correct index
+                        int index = number - front.size();
+                        fio.println(back.get(index));
+                    }
+                    break;
+            }
+
+        }
 
         fio.close(); // important; always close at the end of the code
     }
 
+}
+
+class FakeCircularArrayList {
+    Integer[] items;
+    int firstIndex;
+    int lastIndex;
+    int size = 0;
+
+    FakeCircularArrayList(int totalCapacity) {
+        items = new Integer[totalCapacity];
+        // Set the indexes to be in the middle to not overflow
+        firstIndex = totalCapacity / 2;
+        lastIndex = totalCapacity / 2;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFront(Integer num) {
+        if (this.size() == 0) {
+            items[firstIndex] = num;
+        } else {
+            items[--firstIndex] = num;
+        }
+        size++;
+    }
+
+    public void addBack(Integer num) {
+        if (size == 0) {
+            items[lastIndex] = num;
+        } else {
+            items[++lastIndex] = num;
+        }
+        size++;
+    }
+
+    //To get the actual index of the item
+    public Integer get(int index) {
+        return items[index + firstIndex];
+    }
+
+    public Integer removeFront() {
+        size--;
+        Integer num = items[firstIndex];
+        items[firstIndex] = null;
+        if (this.size() != 0) {
+            firstIndex++;
+        }
+        return num;
+    }
+
+    public Integer removeBack() {
+        size--;
+        Integer num = items[lastIndex];
+        items[lastIndex] = null;
+        if (this.size() != 0) {
+            lastIndex--;
+        }
+        return num;
+    }
 }
 
 /**
