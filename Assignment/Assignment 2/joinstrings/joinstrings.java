ef421760c8afbd154e06f362ab74a94a48d1b0e4
francisyzy <francisyzy@u.nus.edu>
2022-09-21 12:48:36 +0800

    Remove old code

--- Assignment/Assignment 2/joinstrings/joinstrings.java
+++ Assignment/Assignment 2/joinstrings/joinstrings.java
@@ -1,3 +1,4 @@
+***REMOVED***
 import java.io.*;
 import java.util.*;
 
@@ -7,68 +8,31 @@ public class joinstrings {
 
         int numOfStrings = fio.nextInt(); // read int
 
-        // ArrayList<StringBuilder> st rings = new ArrayList<StringBuilder>();
         ArrayList<ListNode> strings = new ArrayList<ListNode>();
         strings.ensureCapacity(numOfStrings);
 
-        // ListNode[] strings = new ListNode[numOfStrings];
-        // TailedLinkedList strings = new TailedLinkedList();
         for (int i = 0; i < numOfStrings; i++) {
-            // strings.addBack(fio.nextLine());
-            // ListNode string = new ListNode(fio.nextLine());
             strings.add(new ListNode(fio.nextLine()));
-            // strings[i] = new ListNode(fio.nextLine());
         }
         // fio.println(strings);
 
         // String operations
-        int lastString = 1;
+        int lastString = 0;
         for (int i = 0; i < numOfStrings - 1; i++) {
-            int first = fio.nextInt() - 1; // read int
-            int second = fio.nextInt() - 1; // read int
-            ListNode toModify = strings.get(first);
-            ListNode toAdd = strings.get(second);
+            int firstIndex = fio.nextInt() - 1; // read int
+            int secondIndex = fio.nextInt() - 1; // read int
+            ListNode toModify = strings.get(firstIndex);
+            ListNode toAdd = strings.get(secondIndex);
             toModify.tail.next = toAdd;
             toModify.tail = toAdd.tail;
-            // ListNode toModify = strings[first];
-            // toModify.getTail().setNext(strings.get(second));
-            // toModify.setTail(strings.get(second).getTail());
-            // toModify.getTail().setNext(strings[second]);
-            // toModify.setTail(strings[second].getTail());
 
-            // strings.set(first, strings.get(first).append(strings.get(second)));
-            // strings.set(second, null);
-            lastString = first;
+            lastString = firstIndex;
         }
-        // ListNode print = strings[lastString];
         ListNode print = strings.get(lastString);
-        // fio.println(lastString);
-        // fio.println(strings);
-        // while (print.getNext() != null) {
-        // fio.print(print.getItem());
-        // print = print.getNext();
-        // }
-        // fio.println(print.getItem());
         while (print != null) {
             fio.print(print.getItem());
             print = print.next;
         }
-        // fio.println("");
-
-        // for (int i = 0; i < numOfStrings; i++) {
-        // if (strings.get(i) != null) {
-        // fio.println(strings.get(i));
-        // }
-        // }
-        // strings.removeAll(Collections.singleton(null));
-        // strings.removeIf(Objects::isNull);
-        // fio.println(strings.get(0));
-
-        // double real = fio.nextDouble(); // read double
-        // String token = fio.next(); // read a single token
-        // String line = fio.nextLine(); // read an entire line
-        // fio.print("..."); // print the "..." contents
-        // fio.println("..."); // print the "..." contents with newline at the end
 
         fio.close(); // important; always close at the end of the code
     }
