fa482e6f1f4efbeecb0f5eddde3db4b0eebe72a7
francisyzy <francisyzy@u.nus.edu>
2022-10-04 18:43:31 +0800

    Init Week 9

---
+++ Assignment/Assignment 3/nicknames/nicknames.java
@@ -0,0 +1,66 @@
+***REMOVED***
+import java.io.*;
+import java.util.*;
+
+public class nicknames {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO(); // create new instance
+
+        int num = fio.nextInt(); // read int
+        double real = fio.nextDouble(); // read double
+        String token = fio.next(); // read a single token
+        String line = fio.nextLine(); // read an entire line
+        fio.print("..."); // print the "..." contents
+        fio.println("..."); // print the "..." contents with newline at the end
+
+        fio.close(); // important; always close at the end of the code
+    }
+}
+
+/**
+ * Fast I/O
+ * 
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastIO() {
+        super(new BufferedOutputStream(System.out));
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
