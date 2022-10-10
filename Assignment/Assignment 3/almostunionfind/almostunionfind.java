33504bfaebe610b365133a78953abeec5b22db68
francisyzy <francisyzy@u.nus.edu>
2022-10-10 21:54:36 +0800

    Attempt 2

--- Assignment/Assignment 3/almostunionfind/almostunionfind.java
+++ Assignment/Assignment 3/almostunionfind/almostunionfind.java
@@ -9,91 +9,136 @@ public class almostunionfind {
         int n = fio.nextInt(); // read int
         int m = fio.nextInt(); // read int
 
-        int[] collections = new int[n + 1]; // 1 index arr
-
-        // System.out.println("original int array : " + Arrays.toString(collections));
-
-        Arrays.setAll(collections, i -> -1);
-        collections[0] = 0;// less confusion
-
-        // System.out.println("array.setAll(int) output: " +
-        // Arrays.toString(collections));
+        DisjointUnionSets disjointUnionSets = new DisjointUnionSets(n);
+        int[] display = new int[n + 1];
+        Arrays.setAll(display, i -> i);
 
         for (int i = 0; i < m; i++) {
-            System.out.println("array.setAll(int) output: " + Arrays.toString(collections));
+            System.out.println("i: " + i);
+            // System.out.println("whole display: " + Arrays.toString(display));
+            // System.out.println("whole array  : " + disjointUnionSets);
+            // System.out.println("whole rank   : " + disjointUnionSets.rankString());
             int opCode = fio.nextInt(); // read int
             int p = fio.nextInt(); // read int
-            System.out.println("p: " + p);
-            int p_parentIndex = collections[p];
-            int p_parentValue = -1;
-            System.out.println("p_parentIndex: " + p_parentIndex);
-            System.out.println("p_parentValue: " + p_parentValue);
-            while (p_parentIndex > 0) {
-            p_parentIndex = collections[p_parentIndex];
-            p_parentValue = collections[p_parentIndex];
-            }
             if (opCode == 3) {
-                // if (p_parentIndex < 0) { // means p is at the root already
-                // p_parentIndex = p;
-                // }
+                // System.out.println(disjointUnionSets.findParentRank(p));
+                // System.out.println(collections[p_parentIndex]);
                 // fio.println(collections[p_parentIndex]); // print the "..." contents with
                 // newline at the end
             } else {
-                // int p_parentIndex = collections[p];
-                // int p_parentValue = -1;
-                // System.out.println("p_parentIndex: " + p_parentIndex);
-                // System.out.println("p_parentValue: " + p_parentValue);
-                // while (p_parentIndex > 0) {
-                //     p_parentIndex = collections[p_parentValue];
-                //     p_parentValue = collections[p_parentIndex];
-                // }
                 int q = fio.nextInt(); // read int
-
-                int q_parentIndex = collections[q];
-                int q_parentValue = -1;
-                System.out.println("q_parentIndex: " + q_parentIndex);
-                System.out.println("q_parentValue: " + q_parentValue);
-                while (q_parentIndex > 0) {
-                    q_parentIndex = collections[q_parentIndex];
-                    q_parentValue = collections[q_parentIndex];
-                }
                 if (opCode == 1) { // union
-                    if (p_parentValue < 0 && q_parentValue < 0) { // means both are roots
-                        collections[p] += collections[q]; // update the number of child nodes
-                        collections[q] = p; // set parent of q into p
-                    } else if (p_parentValue == q_parentValue) { // means both roots are same size
-                        collections[p_parentIndex] += collections[q_parentIndex]; // update the number of child nodes
-                        collections[q_parentIndex] = p_parentIndex; // set parent of q into p
-                    } else if (p_parentValue > q_parentValue) { // means p union is bigger than q
-                        collections[p_parentIndex] += collections[q_parentIndex]; // update the number of child nodes
-                        collections[q_parentIndex] = p_parentIndex; // set parent of q into p
-                    } else if (p_parentValue < q_parentValue) { // means q union is bigger than p
-                        collections[q_parentIndex] += collections[p_parentIndex]; // update the number of child nodes
-                        collections[p_parentIndex] = q_parentIndex; // set parent of q into p
-                    }
-
+                    disjointUnionSets.union(p, q);
                 }
                 if (opCode == 2) { // move
-                    if (p_parentIndex == q_parentIndex) {
-                        // Do nothing because they are the same set already
-                    } else {
-                        if (p_parentIndex < 0) { // means p is at the root already
-                            p_parentIndex = p;
-                        }
-                        if (q_parentIndex < 0) { // means q is at the root already
-                            q_parentIndex = q;
-                        }
-                        // update set sizes
-                        collections[q_parentIndex]--;
-                        collections[p_parentIndex]++;
-                        // move set pointer
-                        collections[p] = q;
-                    }
+                    disjointUnionSets.move(p, q);
                 }
             }
         }
 
         fio.close(); // important; always close at the end of the code
+    }
+}
+
+class DisjointUnionSets {
+    int[] rank, parent;
+    int n;
+
+    // Constructor
+    public DisjointUnionSets(int n) {
+        n++;
+        rank = new int[n];
+        parent = new int[n];
+        Arrays.setAll(this.parent, i -> i);
+        this.n = n;
+    }
+
+    int findSize(int x) {
+
+        return 0;
+    }
+
+    // Returns representative of x's set
+    int findParent(int x) {
+        // Finds the representative of the set
+        // that x is an element of
+        if (parent[x] != x) {
+            // if x is not the parent of itself
+            // Then x is not the representative of
+            // his set,
+            parent[x] = findParent(parent[x]);
+
+            // so we recursively call Find on its parent
+            // and move i's node directly under the
+            // representative of this set
+        }
+
+        return parent[x];
+    }
+
+    void move(int x, int y) {
+        // Find representatives of two sets
+        int x_parent = findParent(x), y_parent = findParent(y);
+
+        // Elements are in the same set, no need
+        // to unite anything.
+        // System.out.println(x_parent);
+        // System.out.println(y_parent);
+        if (x_parent == y_parent) {
+            return;
+        }
+        // Different set, need to move set x into set y.
+        else {
+            parent[x] = y;
+        }
+
+    }
+
+    // Unites the set that includes x and the set
+    // that includes x
+    void union(int x, int y) {
+        // Find representatives of two sets
+        int x_parent = findParent(x), y_parent = findParent(y);
+
+        // Elements are in the same set, no need
+        // to unite anything.
+        if (x_parent == y_parent) {
+            return;
+        }
+
+        // If x's rank is less than y's rank
+        if (rank[x_parent] < rank[y_parent]) {
+            // Then move x under y so that depth
+            // of tree remains less
+            parent[x_parent] = y_parent;
+        }
+
+        // Else if y's rank is less than x's rank
+        else if (rank[y_parent] < rank[x_parent]) {
+            // Then move y under x so that depth of
+            // tree remains less
+            parent[y_parent] = x_parent;
+        }
+
+        else // if ranks are the same
+        {
+            // Then move y under x (doesn't matter
+            // which one goes where)
+            parent[y_parent] = x_parent;
+
+            // And increment the the result tree's
+            // rank by 1
+            rank[x_parent] = rank[x_parent] + 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(parent);
+    }
+
+    public String rankString() {
+        return Arrays.toString(rank);
     }
 }
 
