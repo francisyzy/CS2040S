8294f6ddff6528d036c00e5b04aacce2fa4c99ec
francisyzy <francisyzy@u.nus.edu>
2022-10-11 09:36:54 +0800

    Finish but it is too slow

--- Assignment/Assignment 3/almostunionfind/almostunionfind.java
+++ Assignment/Assignment 3/almostunionfind/almostunionfind.java
@@ -10,20 +10,22 @@ public class almostunionfind {
         int m = fio.nextInt(); // read int
 
         DisjointUnionSets disjointUnionSets = new DisjointUnionSets(n);
-        int[] display = new int[n + 1];
-        Arrays.setAll(display, i -> i);
+        // int[] display = new int[n + 1];
+        // Arrays.setAll(display, i -> i);
 
         for (int i = 0; i < m; i++) {
-            System.out.println("i: " + i);
+            // System.out.println("i: " + i);
             // System.out.println("whole display: " + Arrays.toString(display));
-            // System.out.println("whole array  : " + disjointUnionSets);
-            // System.out.println("whole rank   : " + disjointUnionSets.rankString());
+            // System.out.println("whole array : " + disjointUnionSets);
+            // System.out.println("whole sum : " + disjointUnionSets.sumString());
             int opCode = fio.nextInt(); // read int
             int p = fio.nextInt(); // read int
             if (opCode == 3) {
-                // System.out.println(disjointUnionSets.findParentRank(p));
-                // System.out.println(collections[p_parentIndex]);
-                // fio.println(collections[p_parentIndex]); // print the "..." contents with
+                // System.out.println(disjointUnionSets.findParentsum(p));
+                // System.out.println(parent[p_parentIndex]);
+                fio.print(disjointUnionSets.findParentSize(p));
+                fio.print(' ');
+                fio.println(disjointUnionSets.findSum(p));
                 // newline at the end
             } else {
                 int q = fio.nextInt(); // read int
@@ -41,94 +43,104 @@ public class almostunionfind {
 }
 
 class DisjointUnionSets {
-    int[] rank, parent;
-    int n;
+    int[] sum, parent;
 
     // Constructor
     public DisjointUnionSets(int n) {
         n++;
-        rank = new int[n];
+        sum = new int[n];
         parent = new int[n];
-        Arrays.setAll(this.parent, i -> i);
-        this.n = n;
+        Arrays.setAll(this.parent, i -> -1);
+        Arrays.setAll(this.sum, i -> i);
+        parent[0] = 0;
     }
 
-    int findSize(int x) {
-
-        return 0;
+    int findSum(int x) {
+        int parentIndex = findParentIndex(x);
+        return sum[parentIndex];
     }
 
-    // Returns representative of x's set
-    int findParent(int x) {
-        // Finds the representative of the set
-        // that x is an element of
-        if (parent[x] != x) {
-            // if x is not the parent of itself
-            // Then x is not the representative of
-            // his set,
-            parent[x] = findParent(parent[x]);
+    int findParentIndex(int x) {
+        if (isRoot(x)) { // if I am parent, my own index will be returned
+            return x;
+        } else {
+            int parentIndex = parent[x];
+            int oneBefore = parentIndex;
+            while (parentIndex > 0) {
+                oneBefore = parentIndex;
+                parentIndex = parent[parentIndex];
+            }
+            parent[x] = oneBefore;
+            return oneBefore;
 
-            // so we recursively call Find on its parent
-            // and move i's node directly under the
-            // representative of this set
+            // // Recursively find the representative.
+            // int result = findParentIndex(parent[x]);
+
+            // // We cache the result by moving i’s node
+            // // directly under the representative of this set
+            // parent[x] = result;
+
+            // // And then we return the result
+            // return result;
         }
-
-        return parent[x];
     }
 
-    void move(int x, int y) {
-        // Find representatives of two sets
-        int x_parent = findParent(x), y_parent = findParent(y);
+    int findParentSize(int x) {
+        return Math.abs(parent[findParentIndex(x)]);
+    }
 
-        // Elements are in the same set, no need
-        // to unite anything.
-        // System.out.println(x_parent);
-        // System.out.println(y_parent);
-        if (x_parent == y_parent) {
+    boolean isRoot(int x) {
+        return parent[x] < 0;
+    }
+
+    void remove(int x) {
+        if (isRoot(x)) {
             return;
         }
-        // Different set, need to move set x into set y.
-        else {
-            parent[x] = y;
-        }
-
+        int parentIndex = findParentIndex(x);
+        parent[parentIndex]++; // update parent
+        sum[parentIndex] -= x; // update parent
+        parent[x] = -1; // set itself as root
+        sum[x] = x; // reset its own sum
     }
 
-    // Unites the set that includes x and the set
-    // that includes x
-    void union(int x, int y) {
-        // Find representatives of two sets
-        int x_parent = findParent(x), y_parent = findParent(y);
+    void union(int p, int q) {
+        int p_parentSize = findParentSize(p);
+        int q_parentSize = findParentSize(q);
+        int p_parentIndex = findParentIndex(p);
+        int q_parentIndex = findParentIndex(q);
+        if (isRoot(p) && isRoot(q)) { // means both are roots
+            // Set P to be the parent now
+            parent[p] += parent[q]; // update the number of child nodes
+            parent[q] = p; // set parent of q into p
+            sum[p] += q;
+        } else if (p_parentSize == q_parentSize) { // means both roots are same size
+            parent[p_parentIndex] += parent[q_parentIndex]; // update the number of child nodes
+            parent[q_parentIndex] = p_parentIndex; // set parent of q into p
+            sum[p_parentIndex] += q;
+        } else if (p_parentSize > q_parentSize) { // means p union is bigger than q
+            parent[p_parentIndex] += parent[q_parentIndex]; // update the number of child nodes
+            parent[q_parentIndex] = p_parentIndex; // set parent of q into p
+            sum[p_parentIndex] += q;
+        } else if (p_parentSize < q_parentSize) { // means q union is bigger than p
+            parent[q_parentIndex] += parent[p_parentIndex]; // update the number of child nodes
+            parent[p_parentIndex] = q_parentIndex; // set parent of q into p
+            sum[q_parentIndex] += p;
+        }
+    }
 
-        // Elements are in the same set, no need
-        // to unite anything.
-        if (x_parent == y_parent) {
+    void move(int p, int q) {
+        if (findParentIndex(p) == findParentIndex(q)) {
+            return;// both parents same. Do nothing
+        } else {
+            if (isRoot(p) && isRoot(q)) { // means p & q is root
+                union(p, q);
+                return;
+            }
+
+            remove(p);
+            union(q, p); // add both tgt
             return;
-        }
-
-        // If x's rank is less than y's rank
-        if (rank[x_parent] < rank[y_parent]) {
-            // Then move x under y so that depth
-            // of tree remains less
-            parent[x_parent] = y_parent;
-        }
-
-        // Else if y's rank is less than x's rank
-        else if (rank[y_parent] < rank[x_parent]) {
-            // Then move y under x so that depth of
-            // tree remains less
-            parent[y_parent] = x_parent;
-        }
-
-        else // if ranks are the same
-        {
-            // Then move y under x (doesn't matter
-            // which one goes where)
-            parent[y_parent] = x_parent;
-
-            // And increment the the result tree's
-            // rank by 1
-            rank[x_parent] = rank[x_parent] + 1;
         }
     }
 
@@ -137,8 +149,8 @@ class DisjointUnionSets {
         return Arrays.toString(parent);
     }
 
-    public String rankString() {
-        return Arrays.toString(rank);
+    public String sumString() {
+        return Arrays.toString(sum);
     }
 }
 
