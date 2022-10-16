b84ab4fb30bc9c622cc8e905ea5b06baa5c8d1e3
francisyzy <francisyzy@u.nus.edu>
2022-10-16 22:17:48 +0800

    Works but not Trie and TLE

--- Assignment/Assignment 3/babynames/babynames.java
+++ Assignment/Assignment 3/babynames/babynames.java
@@ -7,25 +7,46 @@ public class babynames {
     public static void main(String[] args) {
         FastIO fio = new FastIO(); // create new instance
 
+        TreeSet<String> boyNames = new TreeSet();
+        TreeSet<String> girlNames = new TreeSet();
         int num = fio.nextInt(); // read int
         while (num != 0) {
             switch (num) {
-                case 1:
+                case 1: // add suggestion
                     String name = fio.next();
                     int gender = fio.nextInt();
+                    if (gender == 1) {
+                        boyNames.add(name);
+                    } else {
+                        girlNames.add(name);
+                    }
                     break;
-                case 2:
+                case 2: // remove suggestion
                     String removeName = fio.next();
-
+                    boyNames.remove(removeName);
+                    girlNames.remove(removeName);
                     break;
-                case 3:
+                case 3: // query
+                    String start = fio.next();
+                    String end = fio.next();
 
+                    int genderSuitability = fio.nextInt();
+                    int totalNum = 0;
+                    if (genderSuitability == 0) {
+                        totalNum += boyNames.subSet(start, end).size();
+                        totalNum += girlNames.subSet(start, end).size();
+                    } else if (genderSuitability == 1) {
+                        totalNum += boyNames.subSet(start, end).size();
+                    } else if (genderSuitability == 2) {
+                        totalNum += girlNames.subSet(start, end).size();
+                    }
+                    fio.println(totalNum); // print the "..." contents with newline at the end
                     break;
 
-                default: // case 4
+                default:
+                    System.out.println("Why are you here");
                     break;
             }
-            fio.println("..."); // print the "..." contents with newline at the end
             num = fio.nextInt(); // read int
         }
 
