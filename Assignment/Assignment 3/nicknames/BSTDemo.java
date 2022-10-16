77c52d03f0620402e9a3513454e83d16e8505d7a
francisyzy <francisyzy@u.nus.edu>
2022-10-16 14:47:25 +0800

    Change the key into String

--- Assignment/Assignment 3/nicknames/BSTDemo.java
+++ Assignment/Assignment 3/nicknames/BSTDemo.java
@@ -6,7 +6,7 @@ class BSTDemo {
     public static void main(String[] args) throws Exception {
         BST T = new BST(); // an empty BST
 
-        int[] sample = new int[] { 15, 23, 6, 71, 50, 4, 7, 5 };
+        String[] sample = new String[] { "Alice", "Bob", "Charlie", "Eve", "Mallory", "AliceA" };
 
         // Sample BST as shown in Lecture
         for (int i = 0; i < sample.length; i++) {
@@ -20,7 +20,7 @@ class BSTDemo {
         System.out.println(T.getRootHeight());
 
         for (int i = 0; i < sample.length; i++) {
-            int thingy = sample[i];
+            String thingy = sample[i];
             System.out.print("Searching " + thingy);
             System.out.print(" ");
             System.out.print(T.search(thingy));
@@ -33,23 +33,8 @@ class BSTDemo {
         System.out.println("Find Min: " + T.findMin()); // 4
         System.out.println("Find Max: " + T.findMax()); // 71
 
-        System.out.println("Find Successor: " + T.successor(23)); // 50
-        System.out.println("Find Successor: " + T.successor(7)); // 15
-        System.out.println("Find Successor: " + T.successor(71)); // -1
-        System.out.println("Find Predecessor: " + T.predecessor(23)); // 15
-        System.out.println("Find Predecessor: " + T.predecessor(7)); // 6
-        System.out.println("Find Predecessor: " + T.predecessor(71)); // 50
-
         T.inorder(); // The BST: 4, 5, 6, 7, 15, 23, 50, 71
 
-        System.out.println("Deleting 5");
-        T.delete(5);
-        System.out.println("Deleting 71");
-        T.delete(71);
-        System.out.println("Deleting 15");
-        T.delete(15);
-
-        T.inorder(); // The BST: 4, 6, 7, 23, 50
     }
 }
 
@@ -57,11 +42,11 @@ class BSTDemo {
 class BSTVertex {
     // all these attributes remain public to slightly simplify the code
     public BSTVertex parent, left, right;
-    public int key;
+    public String key;
     public int height; // will be used in lecture on AVL
     public int size; // will be used in lecture on AVL
 
-    BSTVertex(int v) {
+    BSTVertex(String v) {
         key = v;
         parent = left = right = null;
         height = -1;
@@ -79,20 +64,20 @@ class BST {
     }
 
     // public method called to search for a value v.
-    // Return v if it is found in the BST otherwise return -1.
-    // Here the assumption is that -1 is never a valid key value.
-    public int search(int v) {
+    // Return v if it is found in the BST otherwise return "".
+    // Here the assumption is that "" is never a valid key value.
+    public String search(String v) {
         BSTVertex res = search(root, v);
-        return res == null ? -1 : res.key;
+        return res == null ? "" : res.key;
     }
 
     // helper method to perform search
-    public BSTVertex search(BSTVertex T, int v) {
+    public BSTVertex search(BSTVertex T, String v) {
         if (T == null)
             return null; // not found
         else if (T.key == v)
             return T; // found
-        else if (T.key < v)
+        else if (T.key.compareTo(v) > 0)
             return search(T.right, v); // search to the right
         else
             return search(T.left, v); // search to the left
@@ -101,7 +86,7 @@ class BST {
     // public method called to search for a size v.
     // Return v if it is found in the BST otherwise return -1.
     // Here the assumption is that -1 is never a valid size value.
-    public int getSize(int v) {
+    public int getSize(String v) {
         BSTVertex res = search(root, v);
         return res == null ? -1 : (res.left == null ? 0 : res.left.size) + (res.right == null ? 0 : res.right.size) + 1;
     }
@@ -116,7 +101,7 @@ class BST {
     // public method called to search for a size v.
     // Return v if it is found in the BST otherwise return -1.
     // Here the assumption is that -1 is never a valid size value.
-    public int getHeight(int v) {
+    public int getHeight(String v) {
         BSTVertex res = search(root, v);
         return res == null ? -1
                 : max(res.left == null ? -1 : res.left.height, res.right == null ? -1 : res.right.height) + 1;
@@ -127,13 +112,13 @@ class BST {
     }
 
     // public method called to find Minimum key value in BST
-    public int findMin() {
+    public String findMin() {
         return findMin(root);
     }
 
     // helper method to perform findMin
     // Question: What happens if BST is empty?
-    public int findMin(BSTVertex T) {
+    public String findMin(BSTVertex T) {
         if (T.left == null)
             return T.key; // this is the min
         else
@@ -141,13 +126,13 @@ class BST {
     }
 
     // public method called to find Maximum key value in BST
-    public int findMax() {
+    public String findMax() {
         return findMax(root);
     }
 
     // helper method to perform findMax
     // Question: Again, what happens if BST is empty?
-    public int findMax(BSTVertex T) {
+    public String findMax(BSTVertex T) {
         if (T.right == null)
             return T.key; // this is the max
         else
@@ -155,13 +140,13 @@ class BST {
     }
 
     // public method to find successor to given value v in BST.
-    public int successor(int v) {
+    public String successor(String v) {
         BSTVertex vPos = search(root, v);
-        return vPos == null ? -1 : successor(vPos);
+        return vPos == null ? "" : successor(vPos);
     }
 
     // helper recursive method to find successor to for a given vertex T in BST
-    public int successor(BSTVertex T) {
+    public String successor(BSTVertex T) {
         if (T.right != null) // this subtree has right subtree
             return findMin(T.right); // the successor is the minimum of right subtree
         else {
@@ -172,18 +157,18 @@ class BST {
                 cur = par; // continue moving up
                 par = cur.parent;
             }
-            return par == null ? -1 : par.key; // this is the successor of T
+            return par == null ? "" : par.key; // this is the successor of T
         }
     }
 
     // public method to find predecessor to given value v in BST
-    public int predecessor(int v) {
+    public String predecessor(String v) {
         BSTVertex vPos = search(root, v);
-        return vPos == null ? -1 : predecessor(vPos);
+        return vPos == null ? "" : predecessor(vPos);
     }
 
     // helper recursive method to find predecessor to for a given vertex T in BST
-    public int predecessor(BSTVertex T) {
+    public String predecessor(BSTVertex T) {
         if (T.left != null) // this subtree has left subtree
             return findMax(T.left); // the predecessor is the maximum of left subtree
         else {
@@ -194,7 +179,7 @@ class BST {
                 cur = par; // continue moving up
                 par = cur.parent;
             }
-            return par == null ? -1 : par.key; // this is the successor of T
+            return par == null ? "" : par.key; // this is the successor of T
         }
     }
 
@@ -209,22 +194,23 @@ class BST {
         if (T == null)
             return;
         inorder(T.left); // recursively go to the left
-        System.out.printf(" %d", T.key); // visit this BST node
+        System.out.print(" "); // visit this BST node
+        System.out.print(T.key); // visit this BST node
         inorder(T.right); // recursively go to the right
     }
 
     // public method called to insert a new key with value v into BST
-    public void insert(int v) {
+    public void insert(String v) {
         root = insert(root, v);
     }
 
     // helper recursive method to perform insertion of new vertex into BST
-    public BSTVertex insert(BSTVertex T, int v) {
+    public BSTVertex insert(BSTVertex T, String v) {
         if (T == null) {
             return new BSTVertex(v); // insertion point is found
         }
 
-        if (T.key < v) { // search to the right
+        if (T.key.compareTo(v) > 0) { // search to the right
             T.right = insert(T.right, v);
             T.right.parent = T;
         } else { // search to the left
@@ -283,18 +269,18 @@ class BST {
     }
 
     // public method to delete a vertex containing key with value v from BST
-    public void delete(int v) {
+    public void delete(String v) {
         root = delete(root, v);
     }
 
     // helper recursive method to perform deletion
-    public BSTVertex delete(BSTVertex T, int v) {
+    public BSTVertex delete(BSTVertex T, String v) {
         if (T == null)
             return T; // cannot find the item to be deleted
 
-        if (T.key < v) // search to the right
+        if (T.key.compareTo(v) > 0) // search to the right
             T.right = delete(T.right, v);
-        else if (T.key > v) // search to the left
+        else if (T.key.compareTo(v) < 0) // search to the left
             T.left = delete(T.left, v);
         else { // this is the node to be deleted
             if (T.left == null && T.right == null) // this is a leaf
@@ -306,7 +292,7 @@ class BST {
                 T.left.parent = T.parent;
                 T = T.left; // bypass T
             } else { // has two children, find successor
-                int successorV = successor(v);
+                String successorV = successor(v);
                 T.key = successorV; // replace this key with the successor's key
                 T.right = delete(T.right, successorV); // delete the old successorV
             }
