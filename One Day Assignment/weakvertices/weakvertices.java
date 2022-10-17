a5ba4b35eb2832c3444a48ce198eca00999945a3
francisyzy <francisyzy@u.nus.edu>
2022-10-17 17:02:31 +0800

    Finish weakvertices

--- One Day Assignment/weakvertices/weakvertices.java
+++ One Day Assignment/weakvertices/weakvertices.java
@@ -20,20 +20,49 @@ public class weakvertices {
                     }
                 }
             }
-            
+            boolean[] triangle = new boolean[numOfVertices];
             for (int i = 0; i < numOfVertices; i++) {
-                if (count[i] == 1) {
-                    // Weak!
+                triangle[i] = false;
+                if (count[i] == 1) { // skip checking if the guy only have one connection because no way u can form a
+                                     // triangle using one line
+                    continue;
                 }
-                if (adjMatrix[i][i] == 1) {
+                for (int j = 0; j < numOfVertices; j++) {
+                    for (int k = 0; k < numOfVertices; k++) {
+                        if (i != j && i != k && j != k) {
+                            // If they are the same numbers, they will check if [1][1] == 1 which is never
+                            // going to be true, save compute
+                            if (edgeExist(adjMatrix, i, j) // if 3 edges exist, means there is a triangle
+                                    && edgeExist(adjMatrix, i, k)
+                                    && edgeExist(adjMatrix, j, k)) {
+                                triangle[i] = true;
+                            }
+                        }
+                    }
                 }
-                
+
             }
-            fio.println("..."); // print the "..." contents with newline at the end
-            numOfVertices = fio.nextInt();
+
+            // Prints out the vectors that have no triangles found
+            for (int i = 0; i < triangle.length; i++) {
+                if (!triangle[i]) {
+                    fio.print(i);
+                    fio.print(" ");
+                }
+
+            }
+            fio.println(); // new line to signify end of case
+            numOfVertices = fio.nextInt(); // get input for next test case
         }
 
         fio.close(); // important; always close at the end of the code
+    }
+
+    private static boolean edgeExist(int[][] adjMatrix, int x, int y) {
+        if (adjMatrix[x][y] == 1 && adjMatrix[y][x] == 1) {
+            return true;
+        }
+        return false;
     }
 }
 
