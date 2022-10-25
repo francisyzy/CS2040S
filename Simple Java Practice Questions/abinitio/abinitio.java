01358417a850b16adf8473c3386355f7d5ff6790
francisyzy <francisyzy@u.nus.edu>
2022-10-25 14:17:32 +0800

    add instructions

--- Simple Java Practice Questions/abinitio/abinitio.java
+++ Simple Java Practice Questions/abinitio/abinitio.java
@@ -10,32 +10,57 @@ public class abinitio {
         int v = fio.nextInt(); // vertices
         int e = fio.nextInt(); // edges
         int q = fio.nextInt(); // queries
+        List<IntegerPair> edgeList = new ArrayList<>();
+        for (int i = 0; i < e; i++) {
+            // Add edge to list (Example: 0<->1 unweighted)
+            edgeList.add(new IntegerPair(fio.nextInt(), fio.nextInt()));
+        }
         for (int i = 0; i < q; i++) {
             int instruction = fio.nextInt(); // instructions
             int x, y;
             switch (instruction) {
                 case 1:
-
+                    // If this integer is 1, no integers follow.
+                    // You should add a new vertex labelled V to G. This vertex should not have
+                    // edges to or from any other vertex. V – the current size of G – now increases
+                    // by 1.
+                    v++;
                     break;
                 case 2:
+                    // If this integer is 2, two integers Xi and Yi (0≤Xi,Yi<V; Xi≠Yi) follow.
+                    // You should add a new directed edge connecting vertex Xi to vertex Yi inG. It
+                    // is guaranteed that this edge does not currently exist.
                     x = fio.nextInt();
                     y = fio.nextInt();
-
+                    edgeList.add(new IntegerPair(x, y));
                     break;
                 case 3:
+                    // If this integer is 3, a single integer Xi (0≤Xi<V) follows.
+                    // You should delete all the incoming and outgoing edges of Xi from the graph G.
                     x = fio.nextInt();
-
+                    // edgeList.find(x);//find the edge and delete
                     break;
                 case 4:
+                    // If this integer is 4, two integers Xi and Yi (0≤Xi,Yi<V; Xi≠Yi) follow.
+
+                    // You should remove the directed edge connecting vertex Xi to vertex Yi from G.
+                    // It is guaranteed that this edge currently exists.
                     x = fio.nextInt();
                     y = fio.nextInt();
-
+                    edgeList.remove(new IntegerPair(x, y));
                     break;
                 case 5:
-
+                    // If this integer is 5, no integers follow.
+                    // You should replace G with its transpose G′, defined as follows:For every pair
+                    // of vertices (a,b), the edge from a to b exists in G′ if and only a≠b and the
+                    // edge from b to a exists in G.
                     break;
                 case 6:
+                    // If this integer is 6, no integers follow.
 
+                    // You should replace G with its complement G¯, defined as follows:For every
+                    // pair of vertices (a,b), the edge from a to b exists in G¯ if and only a≠b and
+                    // the edge from a to b does not exist in G.
                     break;
 
                 default:
@@ -44,12 +69,40 @@ public class abinitio {
             }
 
         }
-        fio.println(v); // print the "..." contents with newline at the end
-        for (int i = 0; i < v; i++) {
-            fio.println("..."); // print the "..." contents with newline at the end
+        fio.println(v); // On the first line, output a single integer V, the number of vertices in the
+                        // graph G.
+        for (int i = 0; i < edgeList.size(); i++) {
+            // di, the outdegree of vertex i, and
+            // hi, the hash of the adjacency list of vertex i, defined as follows. Suppose
+            // the vertices in the out-neighbourhood of vertex i are n1<n2<⋯<ndi. Then
+            // hi=70⋅n1+71⋅n2+72⋅n3+⋯+7di−1⋅ndiSince hi can be quite large, you should
+            // output only the remainder after dividing this number by 109+7.
+            fio.println(edgeList.get(i)); // print the "..." contents with newline at the end
         }
 
         fio.close(); // important; always close at the end of the code
+    }
+}
+
+class IntegerPair {
+    int x, y;
+
+    IntegerPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object in) {
+        if (this != in) {
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
     }
 }
 
