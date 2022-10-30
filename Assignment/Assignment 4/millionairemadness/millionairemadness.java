4b5e104d883828a3d851da7cb8f370ebdf78e68d
francisyzy <francisyzy@u.nus.edu>
2022-10-30 16:16:49 +0800

    Finish millionaremadness

--- Assignment/Assignment 4/millionairemadness/millionairemadness.java
+++ Assignment/Assignment 4/millionairemadness/millionairemadness.java
@@ -15,10 +15,163 @@ public class millionairemadness {
                 adjMatrix[i][j] = fio.nextInt();
             }
         }
-        //Calculate the number of times it needs to go up and whats the biggest bump?
-        fio.println("..."); // print the "..." contents with newline at the end
+        ArrayList<ArrayList<IntegerPair>> adjList = adjToList_BFS(adjMatrix, M, N);
+        // Calculate the number of times it needs to go up and whats the biggest bump?
+
+        PriorityQueue<IntegerPair> pq = new PriorityQueue<IntegerPair>();
+        ArrayList<Boolean> taken = new ArrayList<Boolean>();
+        taken.addAll(Collections.nCopies(M * N, false));
+        process(0, adjList, taken, pq);
+        // int mst_cost = 0;
+
+        ArrayList<Integer> largestCost = new ArrayList<Integer>();
+
+        while (!pq.isEmpty()) { // we will do this until all V vertices are taken (or E = V-1 edges are taken)
+            IntegerPair front = pq.poll();
+
+            if (!taken.get(front.y)) { // we have not connected this vertex yet
+                largestCost.add(front.x);
+                // mst_cost += front.x; // add the weight of this edge
+                // System.out.println("Adding edge: (" + front.x + ", " + front.y + "), MST cost
+                // now = " + mst_cost);
+                process(front.y, adjList, taken, pq);
+                // process(front.y);
+                if (front.y == (M * N - 1)) { // reach the last vertex total -1 due to 0 index
+                    break;
+                }
+            } else { // this vertex has been connected before via some other tree branch
+                // System.out.println("Ignoring edge: (" + front.x + ", " + front.y + "), MST
+                // cost now = " + mst_cost);
+            }
+        }
+        // System.out.println(largestCost);
+        Collections.sort(largestCost);
+        Collections.reverse(largestCost);
+        // System.out.println(largestCost);
+        // System.out.println(adjList);
+        if (largestCost.size() == 0) {
+            largestCost.add(0);
+        }
+        fio.println(largestCost.get(0)); // print the "..." contents with newline at the end
 
         fio.close(); // important; always close at the end of the code
+    }
+
+    static ArrayList<ArrayList<IntegerPair>> adjToList_BFS(int[][] adjMatrix, int r, int c) {
+        int numberOfPilersOfGold = r * c;
+
+        ArrayList<ArrayList<IntegerPair>> adjList = new ArrayList<ArrayList<IntegerPair>>();
+        for (int i = 0; i < numberOfPilersOfGold; i++) {
+            adjList.add(new ArrayList<IntegerPair>()); // store blank vector first
+        }
+
+        int countBase = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                final int x = i;
+                final int y = j;
+                int check_x = 0;
+                int check_y = 0;
+                int adjListItem = 0;
+
+                // Check up
+                check_x = x - 1;
+                check_y = y;
+                adjListItem = countBase - c;
+                if (check_x >= 0) {
+                    // System.out.println("Check up");
+                    updateAdjList(adjMatrix, adjList, x, y, check_x, check_y, adjListItem, countBase);
+                }
+                // Check Down
+                check_x = x + 1;
+                check_y = y;
+                adjListItem = countBase + c;
+                if (check_x < r) {
+                    // System.out.println("Check down");
+                    updateAdjList(adjMatrix, adjList, x, y, check_x, check_y, adjListItem, countBase);
+                }
+                // Check Left
+                check_x = x;
+                check_y = y - 1;
+                adjListItem = countBase - 1;
+                if (check_y >= 0) {
+                    // System.out.println("Check left");
+                    updateAdjList(adjMatrix, adjList, x, y, check_x, check_y, adjListItem, countBase);
+                }
+                // Check Right
+                check_x = x;
+                check_y = y + 1;
+                adjListItem = countBase + 1;
+                if (check_y < c) {
+                    // System.out.println("Check right");
+                    updateAdjList(adjMatrix, adjList, x, y, check_x, check_y, adjListItem, countBase);
+                }
+                countBase++;
+            }
+        }
+
+        return adjList;
+    }
+
+    static void updateAdjList(int[][] adjMatrix,
+            ArrayList<ArrayList<IntegerPair>> adjList,
+            int x, int y, int check_x, int check_y, int adjListItem, int countBase) {
+        // System.out.println(Arrays.deepToString(checked));
+        // System.out.println(adjListItem);
+        // System.out.println("check x: " + check_x);
+        // System.out.println("check y: " + check_y);
+        int checkDifference = adjMatrix[check_x][check_y] - adjMatrix[x][y];
+        checkDifference = checkDifference < 0 ? 0 : checkDifference;
+        // System.out.println(checkDifference);
+        adjList.get(countBase).add(new IntegerPair(adjListItem, checkDifference));
+    }
+
+    static void process(int vtx, ArrayList<ArrayList<IntegerPair>> AdjList, ArrayList<Boolean> taken,
+            PriorityQueue<IntegerPair> pq) {
+        // System.out.println(">> At vertex " + vtx);
+        taken.set(vtx, true);
+        for (int j = 0; j < AdjList.get(vtx).size(); j++) {
+            IntegerPair v = AdjList.get(vtx).get(j);
+            if (!taken.get(v.x)) {
+                pq.offer(new IntegerPair(v.y, v.x)); // we sort by weight then by adjacent vertex
+                // System.out.println(">> Inserting (" + v.y + ", " + v.x + ") to priority
+                // queue");
+            } else {
+                // System.out.println(">> Ignoring (" + v.y + ", " + v.x + ")");
+            }
+        }
+    }
+
+}
+
+class IntegerPair implements Comparable<IntegerPair> {
+    int x, y;
+
+    IntegerPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object in) {
+        if (this != in) {
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(IntegerPair in) {
+        if (this.x != in.x) {
+            return this.x - in.x;
+        } else {
+            return this.y - in.y;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
     }
 }
 
